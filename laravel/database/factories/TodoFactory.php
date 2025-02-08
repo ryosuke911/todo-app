a8dae0f5019fe7793a120151efdf1a42af27cb9f
ryosuke911 <ryosuke3028911@gmail.com>
2025-02-08 10:48:24 +0900

    テストデータ生成用のファクトリーファイルを追加

---
+++ laravel/database/factories/TodoFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TodoFactory extends Factory
+{
+    protected $model = Todo::class;
+
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'status' => $this->faker->randomElement(['pending', 'in_progress', 'completed']),
+            'deadline' => $this->faker->dateTimeBetween('now', '+30 days'),
+        ];
+    }
+} 
